e747d5fe5cd9b1d255ce9ed5a94f62884d499e61
vonog99-max <vonog99@gmail.com>
2026-06-28 01:11:44 +0800

    chore: upload capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.idlelegend.game',
+  appName: 'Idle Legend Bang Bang RPG',
+  webDir: 'dist'
+};
+
+export default config;
